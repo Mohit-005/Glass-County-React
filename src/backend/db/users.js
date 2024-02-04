24e007ed25d697ad8b361ef6e61619de4ad37d05
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-02-04 22:57:33 +0530

    update: data for products and address

---
+++ src/backend/db/users.js
@@ -0,0 +1,18 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * Every user will have cart (Quantity of all Products in Cart is set to 1 by default), wishList by default
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    username: "Mohit Kumar",
+    email: "mohitloves@boobs.com",
+    password: "Mohit<3BOOBS",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
