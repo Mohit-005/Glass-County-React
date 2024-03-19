cd7fbbd973a0c595fe79eea3257958ecca872839
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-03-19 14:50:31 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ import {
 const Footer = () => {
   return (
     <div className="py-5 mt-3  bg-amber-50 flex flex-wrap justify-center items-center gap-2 md:gap-10  absolute right-0 left-0 ">
-      <p>Made with my love for BOOBS </p>
+      <p>Made by Mohit </p>
       <p className="flex gap-3">
         <a href="https://github.com/Mohit-005">
           <AiFillGithub className="text-2xl text-gray-800" />
