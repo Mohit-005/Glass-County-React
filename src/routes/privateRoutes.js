25ef6c777b9bdd5f3e6a5479c9696d52dc7b3970
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-10-14 01:03:11 +0530

    add: razorpay payment

---
+++ src/routes/privateRoutes.js
@@ -0,0 +1,25 @@
+import { Cart, Wishlist, Checkout, Profile, Orders } from "../pages";
+
+const privateRoutes = [
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/wishlist",
+    element: <Wishlist />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/orders",
+    element: <Orders />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+];
+export { privateRoutes };
