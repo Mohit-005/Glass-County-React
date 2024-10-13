8c6fd4145f40c89a47acfbab8449e6efaff74adc
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-10-14 01:01:43 +0530

    add: razorpay payment

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+export { default as Signup } from "./Signup";
+export { default as Login } from "./Login";
+
+export { default as Home } from "./Home";
+
+export { default as ProductListing } from "./ProductListing";
+export { default as ProductDetails } from "./ProductDetails";
+export { default as Cart } from "./Cart";
+export { default as Wishlist } from "./Wishlist";
+
+export { default as Profile } from "./Profile";
+
+export { default as Checkout } from "./Checkout";
+export { default as Orders } from "./Orders";
+
+export { default as ErrorPage } from "./ErrorPage";
