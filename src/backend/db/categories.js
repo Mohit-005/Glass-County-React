ed835a582c993484c9eb057cac57787075e9ea39
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-10-14 00:45:21 +0530

    update: added images to category

---
+++ src/backend/db/categories.js
@@ -0,0 +1,32 @@
+import glassCategory1 from "../../assets/categories/visionmod1.jpg";
+import glassCategory2 from "../../assets/categories/sunmod1.jpg";
+import glassCategory3 from "../../assets/categories/sportsmod1.jpg";
+
+/**
+ * Category Database can be added here.
+ * You can add category of your wish with different attributes
+ * */
+
+export const categories = [
+  {
+    _id: "ef1f52f6-4e35-4cd7-ad4a-0a9b52de894f",
+    categoryName: "vision",
+    description:
+      "Clear vision and stylish frames for a perfect blend of functionality and fashion",
+    categoryImg: glassCategory1,
+  },
+  {
+    _id: "937d27a0-51b7-4005-bb04-4f5f111eac90",
+    categoryName: "sports",
+    description:
+      "Stay focused and perform at your best with our high-performance sports glasses.",
+    categoryImg: glassCategory3,
+  },
+  {
+    _id: "1164f45b-1659-4631-88d7-47325bb21eff",
+    categoryName: "sunglasses",
+    description:
+      "Protect your eyes in style with our UV-protected sunglasses collection.",
+    categoryImg: glassCategory2,
+  },
+];
